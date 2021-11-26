69cf604828b356f3af605b88cf02e31b60681a75
Nutzu <nutzu4dev@gmail.com>
2021-11-26 23:43:41 +0200

    created directory/menu-item components + styling

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-import React from 'react';
+import React from "react";
 
-import HomePage from './homepage.component';
+import HomePage from "./pages/homepage/homepage.component";
 
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
