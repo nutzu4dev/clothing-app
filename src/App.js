4ad18e833b41116d2965fe8b59f4dcd7dfc4fccc
Nutzu <nutzu4dev@gmail.com>
2021-11-27 20:06:59 +0200

    sign in component + some styling

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { Switch, Route } from 'react-router-dom';
 
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
+import SignInAndSignUp from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 import Header from './components/header/header.component';
 
 import './App.css';
@@ -14,7 +15,8 @@ function App() {
       <Header />
       <Switch>
         <Route exact path='/' component={HomePage} />
-        <Route exact path='/shop' component={ShopPage} />
+        <Route path='/shop' component={ShopPage} />
+        <Route path='/signIn' component={SignInAndSignUp} />
       </Switch>
     </div>
   );
---
+++ src/components/custom-button/custom-button.component.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import './custom-button.styles.scss';
+
+const CustomButton = ({ children, ...customButtonProps }) => (
+  <button className='custom-button' {...customButtonProps}>
+    {children}
+  </button>
+);
+
+export default CustomButton;
